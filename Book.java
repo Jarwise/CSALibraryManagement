8b59c839718fcc2b0e16b29b0ddb3e3816065036
Jarwise <57717541+Jarwise@users.noreply.github.com>
2020-12-02 17:00:59 +0100

    Book update

--- Book.java
+++ Book.java
@@ -1,24 +1,28 @@
 public class Book {
     private String title = "Untitled";
     private String author = "Unknown";
-    private int ID;
+    private int ID = 1;
     private String ISBN;
     private boolean borrowed = false;
     private Reader borrower;
     private int readTime = 14; //days
     private Date dateBorrowed;
-    private Date dateExpeceted;
+    private Date dateExpected;
 
     //constructor
     public Book(String name){
         this.title = name;
-        //ID is generated when added to library
         this.ISBN = isbn();
     }
-    public Book(String name, String author){
+    public Book(int ID, String name){
         this.title = name;
-        this.author = author;
+        this.ID = ID;
         this.ISBN = isbn();
+    }
+    public Book(int ID, String name, String ISBN){
+        this.ID = ID;
+        this.title = name;
+        this.ISBN = ISBN;
     }
 
     public String toString(){
@@ -27,11 +31,18 @@ public class Book {
     public boolean free(){
         return(!this.borrowed);
     }
+    public int getID(){
+        return(this.ID);
+    }
     public void borrow(Date date){
         this.borrowed = true;
         this.dateBorrowed = date;
+        this.dateExpected = this.dateBorrowed.add(this.readTime);
     }
-    public void returnBook(){this.borrowed = false;}
+    public void returnBook(){
+        this.borrowed = false;
+        this.dateExpected = null;
+    }
     public void setReadTime(int readTime){
         this.readTime = readTime;
     }
